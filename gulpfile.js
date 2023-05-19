fdc6823f7c8f2d91827f420229f607dd4d4f294e
callmeumm <developerroms@gmail.com>
2023-05-19 12:03:16 +0700

    gulp: disable gulp-data

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,11 @@
 var { task, src, dest, series, parallel } = require('gulp');
 var cleanCSS = require('gulp-clean-css');
 var concat = require("gulp-concat");
-var data = require('gulp-data');
+// var data = require('gulp-data');
 var minify = require('gulp-minify');
 var nunjucks = require('gulp-nunjucks');
 var prettier = require('gulp-prettier');
 var sourcemaps = require('gulp-sourcemaps');
-
-var Config = {
-  routes: [
-    {
-      url: 'navbar',
-      title: "Beranda"
-    },
-    {
-      url: 'layanan',
-      title: "Layanan"
-    },
-    {
-      url: 'portofolio',
-      title: "Portofolio"
-    },
-    {
-      url: 'siapa-kami',
-      title: "Siapa Kami?"
-    },
-    {
-      url: 'tim',
-      title: "Team"
-    },
-    {
-      url: 'kontak',
-      title: "Kontak"
-    }
-  ],
-  portofolios: [
-    {
-      index: 0,
-      source: 'https://dummyimage.com/1920x1281/cbd5e1/0c0f0d',
-    },
-    {
-      index: 1,
-      source: 'https://dummyimage.com/1920x1281/cbd5e1/0c0f0d',
-    },
-    {
-      index: 3,
-      source: 'https://dummyimage.com/1920x1281/cbd5e1/0c0f0d',
-    }
-  ]
-};
 
 task('lib', () => {
   return src('./src/lib/**/*.{js,css,map,eot,svg,ttf,woff}')
@@ -78,7 +35,7 @@ task('custom-js', () => {
 
 task('build-nunjucks', () => {
   src('src/index.njk')
-    .pipe(data(() => Config))
+    // .pipe(data(() => Config))
     .pipe(nunjucks.compile({ }))
     .pipe(prettier({ singleQuote: true }))
     .pipe(dest('dist'))
