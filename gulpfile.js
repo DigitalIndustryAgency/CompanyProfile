dd1a3da02b027ebac52c554437adc42f64d86bc0
callmeumm <developerroms@gmail.com>
2023-05-19 12:17:54 +0700

    chore: remove lib, custom-css, custom-js task

--- gulpfile.js
+++ gulpfile.js
@@ -9,53 +9,32 @@ var nunjucks = require('gulp-nunjucks');
 var prettier = require('gulp-prettier');
 var sourcemaps = require('gulp-sourcemaps');
 
-task('lib', () => {
-  return src('./src/lib/**/*.{js,css,map,eot,svg,ttf,woff}')
-    .pipe(dest('dist/lib'));
-});
-
+// image assets
 task('image', () => {
   return src('./src/images/**/*.{jpg,png}')
     .pipe(dest('dist/assets/img'));
 });
 
-task('custom-css', () => {
-  return src('./src/custom/**/*.css')
-    .pipe(concat('style.min.css'))
-    .pipe(sourcemaps.init())
-    .pipe(cleanCSS())
-    .pipe(sourcemaps.write())
-    .pipe(dest('dist/assets/css'));
-})
-
-task('custom-js', () => {
-  return src('./src/custom/**/*.js')
-    .pipe(concat('script.js'))
-    .pipe(minify())
-    .pipe(dest('dist/assets/js'));
-})
-
+// nunjucks
 task('build-nunjucks', () => {
   return src('src/index.njk')
     // .pipe(data(() => Config))
     .pipe(nunjucks.compile({ }))
     .pipe(prettier({ singleQuote: true }))
     .pipe(dest('dist'))
-})
+});
 
 task('prod-clean-build', () => {
   return src("dist", { read: false, allowEmpty: true }).pipe(
     clean()
   );
-})
+});
 
+// default
 exports.default = series(
   task('prod-clean-build'),
 	parallel(
-		task('lib'),
     task('image'),
-    task('custom-css'),
-    task('custom-js'),
     task('build-nunjucks')
 	)
 );
